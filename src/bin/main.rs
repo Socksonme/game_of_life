0b3f2293ba5e1d0c2dd54ad7fcd14175fd1b6935
socksonme <denislav.dx@gmail.com>
2021-11-26 08:39:31 +0200

    Removed input and output

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,5 @@
 use game_of_life::life::ConwayEngine;
-
 fn main() {
-    match ConwayEngine::from_input() {
-        Ok(mut engine) => {
-            if let Err(err) = engine.run() {
-                eprintln!("Error while getting input: {}", err);
-                std::process::exit(1);
-            }
-        }
-        Err(e) => {
-            eprintln!("Error while getting input: {}", e);
-            std::process::exit(1);
-        }
-    }
+    let mut engine = ConwayEngine::new(1, 1);
+    engine.run();
 }
